e2b82d85031ef07c8684d0a53a22aefdde98871d
Jonathan <scrboy89@gmail.com>
2021-05-26 23:09:41 -0700

    added the js logic to the app

--- counter.js
+++ counter.js
@@ -6,4 +6,52 @@ const counter = document.querySelector("#counter");
 
 
 
+let count = 0;
+
+counter.innerHTML = `${count}`
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", (event)=>{ 
+        const currentBtnClass = event.currentTarget.classList;
+    
+        if(currentBtnClass.contains("btnDecrease")){
+            count = count -1
+            counter.innerHTML =count;
+        }
+        if(currentBtnClass.contains("btnIncrease")){
+            count = count + 1
+            counter.innerHTML =count;
+        }
+        if(currentBtnClass.contains("btnReset")){
+            count = 0
+            counter.innerHTML = count;
+        }
+
+        if(count < 0 ){
+            counter.style.color = "red"
+        }
+        else if(count > 0){
+            counter.style.color = "green"
+        }
+        else {
+            counter.style.color= "black"
+        }
+
+        // testing random stuff
+
+        // function even(count){
+        //   if(count % 2 === 0){
+        //       console.log("even");
+        //   }else {
+        //       console.log("odd")
+        //   } 
+        // }
+
+        // even(count)
+    
+    });
+
+
+});
+
 
